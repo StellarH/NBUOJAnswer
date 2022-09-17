b4d58bbd7ae1bb7f881b9eb0212c3b2af2e46875
Blazing_Stellar <107297301+StellarH@users.noreply.github.com>
2022-09-17 15:48:20 +0800

    Add files via upload

---
+++ 1100/nbuoj1101.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	double a = 1, b = 2, ans = 0;
+	scanf("%d", &n);
+	while(n--) {
+		ans += b/a;
+		b = b + a;
+		a = b - a;
+	}
+	printf("%.6lf\n", ans);
+}
+
